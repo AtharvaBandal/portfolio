5c21e0cf85197225154dd6b14f0115469773bb60
Atharva Bandal <atharvabandal@Atharvas-MacBook-Air-2.local>
2023-08-06 23:46:10 +0530

    1.2

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -6,8 +6,7 @@ import JavaScript from '../assets/javascript.png';
 import ReactImg from '../assets/react.png';
 import Node from '../assets/node.png';
 
-import GitHub from '../assets/github.png';
-import Tailwind from '../assets/tailwind.png';
+
 import Mongo from '../assets/mongo.png';
 
 const Skills = () => {
@@ -17,7 +16,7 @@ const Skills = () => {
             <div className='max-w-[1000px] mx-auto pl-14 flex flex-col justify-center w-full h-full '>
                 <div className='pl-6 ' >
                     <p className='text-4xl font-bold inline  border-b-4 border-bg-[#3a4658]'>Skills</p>
-                    <p className='py-4 '>// The technologies I've worked with....</p>
+                    <p className='py-4 '>The technologies I've worked with....</p>
                 </div>
 
                 <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center pr-10 '>
