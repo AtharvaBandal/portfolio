a075423cc7f9cae0a0efa3227c991ee221818240
Atharva Bandal <bandalatharva@gmail.com>
2024-05-15 18:27:46 +0530

    skills updated

--- src/components/work.jsx
+++ src/components/work.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import natours from '../assets/natours.png';
 import talk from '../assets/talk.png';
-import byte from '../assets/byte.png'
+import byte from '../assets/byte.png';
+import retina from '../assets/retina.png';
 const Work = () => {
   return (
     <div name='projects' className=' w-full md:h-screen bg-[#0a192f] text-gray-300'>
@@ -27,11 +28,11 @@ const Work = () => {
                     <div className='opacity-0 group-hover:opacity-100'>   
                         
                         <div className='pt-[190px]  md:pt-[150px] text-center'>
-                            <a href="http://natours-atharva.onrender.com/">
+                            <a  target="_blank" rel="noopener noreferrer" href="http://natours-atharva.onrender.com/">
                                 <button className='text-center px-4 rounded-lg py-3 m-2 bg-[#040c16] font-bold text-lg text-gray-100'>Explore</button>
                             </a>
 
-                            <a href="https://github.com/AtharvaBandal/Natours">
+                            <a target="_blank" rel="noopener noreferrer" href="https://github.com/AtharvaBandal/Natours">
                                 <button className='text-center px-4 rounded-lg py-3 m-2 bg-white font-bold text-lg text-gray-800'>Code</button>
                             </a>
                         </div>
@@ -51,11 +52,11 @@ const Work = () => {
                         </span> */}
                         
                         <div className='pt-[190px]  md:pt-[150px] text-center'>
-                            <a href="https://project-talk-cn6eypksd-atharvabandal.vercel.app/">
+                            <a target="_blank" rel="noopener noreferrer" href="https://project-talk-cn6eypksd-atharvabandal.vercel.app/">
                                 <button className='text-center px-4 rounded-lg py-3 m-2 bg-[#040c16] font-bold text-lg text-gray-100'>Explore</button>
                             </a>
 
-                            <a href="https://github.com/AtharvaBandal/project_talk">
+                            <a target="_blank" rel="noopener noreferrer" href="https://github.com/AtharvaBandal/project_talk">
                                 <button className='text-center px-4 rounded-lg py-3 m-2 bg-white font-bold text-lg text-gray-800'>Code</button>
                             </a>
                         </div>
@@ -74,11 +75,38 @@ const Work = () => {
                         </span> */}
                         
                         <div className='pt-[190px]  md:pt-[150px] text-center'>
-                            <a href="https://byte-solvers.netlify.app/">
+                            <a target="_blank" rel="noopener noreferrer" href="https://byte-solvers.netlify.app/">
                                 <button className='text-center px-4 rounded-lg py-3 m-2 bg-[#040c16] font-bold text-lg text-gray-100'>Explore</button>
                             </a>
 
-                            <a href="https://github.com/AtharvaBandal/byte-solver-backend">
+                            <a target="_blank" rel="noopener noreferrer" href="https://github.com/AtharvaBandal/byte-solver-backend">
+                                <button className='text-center px-4 rounded-lg py-3 m-2 bg-white font-bold text-lg text-gray-800'>Code</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+
+
+
+                {/* grid Item */}
+                
+                <div 
+                    style={{ backgroundImage: `url(${retina})`}}  
+                    className='shadow-lg shadow-[#040c16] group container rounded-lg flex justify-center items-center mx-auto content-div'
+                >
+                    {/* Hover effect */}
+
+                        {/* <span className='text-2xl font-bold text-black tracking-wider '>
+                            Tour application
+                        </span> */}
+                    <div className='opacity-0 group-hover:opacity-100'>   
+                        
+                        <div className='pt-[190px]  md:pt-[150px] text-center'>
+                            <a target="_blank" rel="noopener noreferrer" href="https://retinal-segmentation.netlify.app/">
+                                <button className='text-center px-4 rounded-lg py-3 m-2 bg-[#040c16] font-bold text-lg text-gray-100'>Explore</button>
+                            </a>
+
+                            <a target="_blank" rel="noopener noreferrer" href="https://github.com/AtharvaBandal/Retina_backend">
                                 <button className='text-center px-4 rounded-lg py-3 m-2 bg-white font-bold text-lg text-gray-800'>Code</button>
                             </a>
                         </div>
